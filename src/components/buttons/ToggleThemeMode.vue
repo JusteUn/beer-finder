1fb6cd22550f10b25f181e006f9de0b3e08fbe8d
Justin Menard <justin.menard@talentsquare.com>
2024-05-29 15:15:11 +0200

    Initial Commit

---
+++ src/components/buttons/ToggleThemeMode.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { useColorMode } from '@vueuse/core'
+import { Icon } from '@iconify/vue'
+import { Button } from '@/components/ui/button'
+
+const mode = useColorMode()
+
+function onChange() {
+  if (mode.value == 'light') {
+    mode.value = 'dark'
+  } else if (mode.value == 'dark') {
+    mode.value = 'light'
+  }
+}
+</script>
+
+<template>
+  <Button variant="ghost" @click="onChange">
+    <Icon
+      icon="radix-icons:moon"
+      class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+    />
+    <Icon
+      icon="radix-icons:sun"
+      class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+    />
+    <span class="sr-only">Toggle theme</span>
+  </Button>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ToggleThemeMode } from '@/components/buttons'
+</script>
+
+<template>
+  <main>
+    <div class="flex h-screen">
+      <div class="m-auto">
+        <h1 class="text-4xl font-extrabold tracking-tight">En Construction</h1>
+      </div>
+    </div>
+    <ToggleThemeMode class="fixed bottom-2 left-2" />
+  </main>
+</template>
